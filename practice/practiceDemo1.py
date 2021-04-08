61ca54371ee7241c9ea5b4fedbc2be67a78fdbd3
nachunrui <ncr0119@163.com>
2021-04-09 01:49:46 +0800

    Study Day 3

--- practice/practiceDemo1.py
+++ practice/practiceDemo1.py
@@ -1,2 +1,9 @@
 # author: nachunrui
 # createTime: 2021/4/8 12:16
+# text_io = open("D:/Jetbrains/WorkSpace/PythonLearning/practice/test.txt", "a+")
+# print("hello world next", file=text_io)
+# text_io.close()
+
+print("1", "2")
+print("1" + "2")
+print(True + False)
